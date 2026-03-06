e1aa9c8aa2186090eecbb0e4aa99a4d0b65ea196
Cristian Alvarez <cristianalvarez280@gmail.com>
2026-03-05 19:25:12 -0500

    Primer commit

---
+++ src/main/java/cesde/Util.java
@@ -0,0 +1,16 @@
+package cesde;
+
+public class Util {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+    student.getId();
+
+        System.out.println("id: " + student.getId());
+
+
+    }
+
+}
